bab6dccc68cb5e96abb3b8f31e3234ac206143b2
Krystian <krystian.pys@gmail.com>
2022-06-24 18:54:51 +0200

    24.06 7kyu

---
+++ codewars/7kyu/categorize-new-member.js
@@ -0,0 +1,14 @@
+// Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+// Output
+// Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+
+// Example
+// input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+// output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+
+
+
+function openOrSenior(data){
+    return data.map(x => x[0] >= 55 && x[1] > 7 ? 'Senior' : 'Open');
+  }
