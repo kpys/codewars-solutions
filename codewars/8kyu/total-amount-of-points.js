161dda16f5e82ed55d2694c885c0dc7ce782b907
Krystian <krystian.pys@gmail.com>
2022-07-05 23:04:42 +0200

    05.07 8kyu

---
+++ codewars/8kyu/total-amount-of-points.js
@@ -0,0 +1,37 @@
+// Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+// if x > y: 3 points
+// if x < y: 0 point
+// if x = y: 1 point
+// Notes:
+
+// there are 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+
+
+function points(games) {
+    let xPoints = 0
+    let yPoints = 0
+    let mapped = games.forEach((x,i) =>{
+      x.split('')
+      
+      let xTeam = x[0]
+      let yTeam = x[2]
+      
+      if (xTeam > yTeam){
+        xPoints += 3;
+      } else if (yTeam > xTeam){
+        yPoints += 3
+      } else {
+        xPoints += 1
+        yPoints += 1
+      }
+    })
+    return xPoints
+  }
