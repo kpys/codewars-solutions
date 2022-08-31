41b63cd63a7ed8066bd4a0010d4afd1cde249771
Krystian <krystian.pys@gmail.com>
2022-08-31 22:21:23 +0200

    31.08 7kyu

---
+++ codewars/7kyu/find-the-stray-number.js
@@ -0,0 +1,15 @@
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+// Complete the method which accepts such an array, and returns that single different number.
+
+// The input array will always be valid! (odd-length >= 3)
+
+// Examples
+// [1, 1, 2] ==> 2
+// [17, 17, 3, 17, 17, 17, 17] ==> 3
+
+
+function stray(numbers) {
+    const nums = numbers.sort((a,b) => a-b)
+    return nums[0] === nums[1] ? nums[nums.length -1] : nums[0]  
+  }
