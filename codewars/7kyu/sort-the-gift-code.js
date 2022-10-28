7899125bc7071ee7a350b425e8bf2a0fdb8bc375
Krystian <krystian.pys@gmail.com>
2022-10-28 09:53:57 +0200

    28.10 7kyu

---
+++ codewars/7kyu/sort-the-gift-code.js
@@ -0,0 +1,16 @@
+// Santa asked his organizer to order the characters in alphabetical order, but the Elf fell asleep from consuming too much hot chocolate and candy canes! Can you help him out?
+
+// Sort the Gift Code
+// Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
+
+// Examples (Input -- => Output):
+// "abcdef"                      -- => "abcdef"
+// "pqksuvy"                     -- => "kpqsuvy"
+// "zyxwvutsrqponmlkjihgfedcba"  -- => "abcdefghijklmnopqrstuvwxyz"
+
+
+function sortGiftCode(code){
+    return code.split('').map((x,i) => code.charCodeAt(i))
+                         .sort((a,b) => a -b).map(x => String.fromCharCode(x))
+                         .join('')
+    }
