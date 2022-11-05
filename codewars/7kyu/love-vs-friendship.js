b42d0887deaa85de1755e5a29fed3c9ddecd51a8
Krystian <krystian.pys@gmail.com>
2022-11-05 23:24:50 +0100

    05.11 7kyu

---
+++ codewars/7kyu/love-vs-friendship.js
@@ -0,0 +1,16 @@
+// If　a = 1, b = 2, c = 3 ... z = 26
+
+// Then l + o + v + e = 54
+
+// and f + r + i + e + n + d + s + h + i + p = 108
+
+// So friendship is twice as strong as love :-)
+
+// Your task is to write a function which calculates the value of a word based off the sum of the alphabet positions of its characters.
+
+// The input will always be made of only lowercase letters and will never be empty.
+
+function wordsToMarks(string){
+    let alphabet =  'A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z'.toLowerCase().split(', ') 
+    return string.split('').map(x => alphabet.indexOf(x) + 1).reduce((tot,current) => tot + current,0)
+  }
