34d414688940d078b9685b5cd8fe975891522020
Krystian <krystian.pys@gmail.com>
2023-01-16 15:12:56 +0100

    16.01 7kyu

---
+++ codewars/7kyu/password-validator.js
@@ -0,0 +1,30 @@
+// Description
+// Your job is to create a simple password validation function, as seen on many websites.
+
+// The rules for a valid password are as follows:
+
+// There needs to be at least 1 uppercase letter.
+// There needs to be at least 1 lowercase letter.
+// There needs to be at least 1 number.
+// The password needs to be at least 8 characters long.
+// You are permitted to use any methods to validate the password.
+
+// Examples:
+// password("Abcd1234"); ===> true
+// password("Abcd123"); ===> false
+// password("abcd1234"); ===> false
+// password("AbcdefGhijKlmnopQRsTuvwxyZ1234567890"); ===> true
+// password("ABCD1234"); ===> false
+// password("Ab1!@#$%^&*()-_+={}[]|\:;?/>.<,"); ===> true;
+// password("!@#$%^&*()-_+={}[]|\:;?/>.<,"); ===> false;
+// Extra info
+// You will only be passed strings.
+// The string can contain any standard keyboard character.
+// Accepted strings can be any length, as long as they are 8 characters or more.
+
+function password(str) {
+    const upper = str.match(/[A-Z]/g) || 0
+    const lower = str.match(/[a-z]/g) || 0
+    const num = str.match(/[0-9]/g) || 0
+    return upper.length > 0 && lower.length > 0 && num.length > 0 && str.length > 7
+  }
