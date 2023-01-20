3efa95c7621e535b3fe2faa7156193bf3a4c25ea
Krystian <krystian.pys@gmail.com>
2023-01-20 10:25:13 +0100

    20.01 7kyu

---
+++ codewars/6kyu/replace-with-alphabet-position.js
@@ -0,0 +1,23 @@
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+
+
+function alphabetPosition(text) {
+    const alph = 'A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z'.toLowerCase().split(', ') 
+    let str = text.replace(/[^a-z]/gi,'')
+                  .split('')
+                  .map(x => x.toLowerCase())
+                  .map(x => alph.indexOf(x) + 1)
+                  .join(' ')
+    return str
+  }
