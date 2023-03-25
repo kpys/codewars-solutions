5428728fb8c36eb2c1a0c5f6cd55da2207561ebf
Krystian <krystian.pys@gmail.com>
2023-03-25 14:25:23 +0100

    25.03

---
+++ codewars/7kyu/say-hello-world-js-style.js
@@ -0,0 +1,12 @@
+// Write the definition of the function "say" such that calling this:
+
+// say("Hello")("World")
+// returns "Hello World"
+
+
+
+var say = (string1) => {
+    return (string2) =>{
+      return string1 + ' ' + string2
+    }
+  }
