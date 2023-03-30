a5f43b14cc1a8d8d48f8be568b7249dbd7d7dcb4
Krystian <krystian.pys@gmail.com>
2023-03-30 18:19:50 +0200

    30.03

---
+++ codewars/7kyu/array-comparator.js
@@ -0,0 +1,5 @@
+// You have two arrays in this kata, every array contains unique elements only. Your task is to calculate number of elements in the first array which are also present in the second array.
+
+function matchArrays(v,r){
+    return v.filter(x => r.includes(x)).length
+  }
