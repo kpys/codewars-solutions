cfcbb2bab5d5f4ecbd44dccc527d08bf0d9cc2e8
Krystian <krystian.pys@gmail.com>
2023-04-14 15:28:13 +0200

    14.04

---
+++ codewars/6kyu/more-zeros-than-ones.js
@@ -0,0 +1,27 @@
+// Create a moreZeros function which will receive a string for input, and return an array (or null terminated string in C) containing only the characters from that string whose binary representation of its ASCII value consists of more zeros than ones.
+
+// You should remove any duplicate characters, keeping the first occurrence of any such duplicates, so they are in the same order in the final array as they first appeared in the input string.
+
+// Examples
+
+// 'abcde' === ["1100001", "1100010", "1100011", "1100100", "1100101"]
+//                True       True       False      True       False
+                   
+//         --> ['a','b','d']
+    
+// 'DIGEST'--> ['D','I','E','T']
+// All input will be valid strings of length > 0. Leading zeros in binary should not be counted.
+
+
+function moreZeros(s){
+    s = [...new Set(s)].map(x => x.charCodeAt(0).toString(2))
+    let arr = []
+    for(let i = 0; i < s.length; i++){
+      let zero = s[i].split('').filter(x => x === '0').length
+      let one = s[i].split('').filter(x => x === '1').length
+      if(zero > one){
+        arr.push(s[i])
+      }
+    }
+      return arr.map(x =>String.fromCharCode(parseInt(x, 2)))
+  }
